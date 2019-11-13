0cbc08380ef5af526f897df159608bc6c778d1b9
mathe <mathe@LAPTOP-T6V958U2>
2019-11-13 17:24:28 -0200

    c1

---
+++ src/ClassTeste.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mathe
+ */
+public class ClassTeste {
+     public static void main(String[] args) {
+        System.out.println("Teste aqr2 1");
+        System.out.println("Teste aqr2 2");
+        System.out.println("Teste aqr2 3");
+    }
+}
---
+++ src/testearqgit/TesteArqGit.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testearqgit;
+
+/**
+ *
+ * @author mathe
+ */
+public class TesteArqGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Teste aqr1 1");
+        System.out.println("Teste aqr1 2");
+        System.out.println("Teste aqr1 3");
+        
+    }
+    
+}
